3a0a869c127a6b5c5dd232cfa3f9d65b716add51
Abhimanyu <abhimanyukarki0@gmail.com>
2026-07-14 17:58:21 -0500

    update-python-example

--- sample_kalshi_use.py
+++ sample_kalshi_use.py
@@ -7,20 +7,26 @@ import kalshi_orderbook
 os.environ["KALSHI_API_KEY_ID"] = "YOUR_API_KEY"
 os.environ["KALSHI_PRIVATE_KEY_PATH"] = "YOUR_PATH_TO_PRIVATE_KEY_FILE"
 
-def main():
-    book = kalshi_orderbook.OrderBook("KXBTCMINY-27JAN01-60000.00")
+MARKET_TICKERS = [
+    "KXBTCMINY-27JAN01-60000.00",
+    "KXBTCMAX100-26-DEC",
+]
 
-    print("Background thread started to maintain the OrderBook")
+def main():
+    book = kalshi_orderbook.OrderBook(MARKET_TICKERS)
+
+    print("Background thread started to maintain the orderBooks")
 
     while True:
-        snapshot = book.get_best_bid_ask()
+        for ticker in MARKET_TICKERS:
+            snapshot = book.get_best_bid_ask(ticker)
 
-        if not snapshot.is_synced:
-            time.sleep(0.1)
-            continue
-            
-        print(f"Live Market -> YES Bid: {snapshot.yes_bid} | YES Ask: {snapshot.yes_ask}")
-        time.sleep(0.5)
+            if not snapshot.is_synced:
+                time.sleep(0.1)
+                continue
+                
+            print(f"{ticker} -> YES Bid: {snapshot.yes_bid} | YES Ask: {snapshot.yes_ask}")
+            time.sleep(0.5)
 
 if __name__ == "__main__":
     main()
