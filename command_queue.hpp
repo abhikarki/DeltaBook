4d8e26d528ba9479186bd26b04964b3159f635b3
Abhimanyu <abhimanyukarki0@gmail.com>
2026-07-25 22:32:52 -0500

    helper-functions

--- command_queue.hpp
+++ command_queue.hpp
@@ -17,6 +17,16 @@ struct Command{
 
 class CommandQueue{
     public:
+        void push(Command cmd){
+            std::lock_guard<std::mutex> lock(mu_);
+            queue_.push_back(std::move(cmd));
+        }
+
+        void drain(std::vector<Command>& out){
+            std::lock_guard<std::mutex> lock(mu_);
+            out.insert(out.end(), std::make_move_iterator(queue_.begin()), std::make_move_iterator(queue_.end()));
+            queue_.clear();
+        }
     private:
         std::mutex mu_;
         std::deque<Command> queue_;
